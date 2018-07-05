1ddaa8fb117e339407895229156b45e021374b5b
Pamela Frieze <pfrieze@gmail.com>
2018-07-05 19:29:16 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,7 +4,7 @@ class Pirate
   PIRATES = []
 
   def initialize(args)
-    @name = params[:name]
+    @name = args[:name]
     @weight = params[:weight]
     @height = params[:height]
     PIRATES << self
